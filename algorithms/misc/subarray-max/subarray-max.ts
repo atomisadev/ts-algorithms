779cf3271371f9a7c4940ad2d3cc3e3f8a34fa98
Atom <atomdevelops@protonmail.com>
2022-09-03 21:32:28 -0400

    Initialized and added Maximum subarray

---
+++ algorithms/misc/subarray-max/subarray-max.ts
@@ -0,0 +1,74 @@
+import { range, reverseRange } from "../../../utils";
+
+/**
+ * Find the best maximum subarray crossing the mid
+ * Time complexity: O(n)
+ * @param input - The input array
+ * @param start - Start of the first array
+ * @param mid - End of first array
+ * @param end - End of second array
+ */
+export function maxCrossSubarray(
+  input: number[],
+  start: number,
+  mid: number,
+  end: number
+): { start: number; end: number; sum: number } {
+  let leftIndex = -1;
+  let leftMaxSum = -Infinity;
+  let sum = 0;
+
+  reverseRange(mid, start).forEach((index) => {
+    sum += input[index];
+    if (sum > leftMaxSum) {
+      leftMaxSum = sum;
+      leftIndex = index;
+    }
+  });
+
+  let rightIndex = -1;
+  let rightMaxSum = -Infinity;
+  sum = 0;
+  range(mid + 1, end).forEach((index) => {
+    sum += input[index];
+    if (sum > rightMaxSum) {
+      rightMaxSum = sum;
+      rightIndex = index;
+    }
+  });
+
+  return {
+    start: leftIndex,
+    end: rightIndex,
+    sum: leftMaxSum + rightMaxSum,
+  };
+}
+
+/**
+ * Returns contiguous subarray within an array of numbers possessing the largest sum.
+ * @timeComplexity O(nlogn) --> O(n*log(n)).
+ * @param input - The input array
+ * @param start - Left side of the subarray
+ * @param end - Right side of the subarray
+ */
+
+export function maxSubarray(
+  input: number[],
+  start: number,
+  end: number
+): { start: number; end: number; sum: number } {
+  if (end - start === 0) return { start, end, sum: input[start] };
+
+  const mid = Math.floor((start + end) / 2);
+  const leftMax = maxSubarray(input, start, mid);
+  const rightMax = maxSubarray(input, mid + 1, end);
+  const crossMax = maxCrossSubarray(input, start, mid, end);
+
+  if (leftMax.sum >= rightMax.sum && leftMax.sum >= crossMax.sum) {
+    return leftMax;
+  } else if (rightMax.sum >= leftMax.sum && rightMax.sum >= crossMax.sum) {
+    return rightMax;
+  } else {
+    return crossMax;
+  }
+}
---
+++ utils/index.ts
@@ -0,0 +1,73 @@
+import baseRange from "lodash/range";
+import baseRangeRight from "lodash/rangeRight";
+
+export function fill<T>(array: T[], valueFn: (index: number) => any): T[] {
+  for (let i = 0; i < array.length; i++) array[i] = valueFn(i);
+
+  return array;
+}
+
+export function increaseOfPrevious(array: number[], index: number) {
+  array[index] += array[index - 1];
+}
+
+/**
+ * Move the item at given index to the right
+ */
+export function moveRight(input, index) {
+  input[index + 1] = input[index];
+}
+
+/**
+ * Creates an array of numbers from start to end, both included.
+ */
+export function range(from: number, to: number): number[] {
+  return baseRange(from, to + 1);
+}
+
+/**
+ * Creates an array of numbers in descending order from start to end, both included.
+ */
+export function reverseRange(from: number, to: number = 0): number[] {
+  return baseRangeRight(to, from + 1);
+}
+
+export interface Node {
+  left: Node;
+  right: Node;
+}
+
+/**
+ * Convert a tree to serializable Binary Tree for Jest snapshots
+ */
+export function toSerializableTree(
+  root: Node,
+  nodeConverter: (node: any) => Node
+): Node {
+  if (!root) return;
+
+  toSerializableTree(root.left, nodeConverter);
+  Object.assign(root, nodeConverter(root));
+  toSerializableTree(root.right, nodeConverter);
+
+  return root;
+}
+
+export function setHead(input: number[], value: number): number[] {
+  input[0] = value;
+  return input;
+}
+
+/**
+ * Swap two items in an array
+ * @param array Input array, whose elements are swapped
+ * @param from From index
+ * @param to Destination index
+ */
+export function swap(input: number[], from: number, to: number): number[] {
+  const temp = input[from];
+  input[from] = input[to];
+  input[to] = temp;
+
+  return input;
+}
